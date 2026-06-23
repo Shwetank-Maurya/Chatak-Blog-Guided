f2aaefbf15e39f019edfcd98ef6bfe4aecaf9d81
Shwetank-Maurya <sd3564086@gmail.com>
2026-06-23 13:41:15 +0530

    application complete now fixing the bugs

--- src/components/index.js
+++ src/components/index.js
@@ -9,6 +9,8 @@ import Login from "./Login";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+import Select from "./Select";
 export  {
     Header,
     Footer,
@@ -21,4 +23,6 @@ export  {
     PostForm,
     PostCard,
     AuthLayout,
+    Input,
+    Select,
 }
