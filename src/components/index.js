6829ae82d0e87634450ed103742b3f89f5b54d84
Shwetank-Maurya <sd3564086@gmail.com>
2026-06-23 12:31:55 +0530

    added form and slug-values

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,18 @@ import Header from "./Header/Header";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogOutBtn from "./Header/LogOutBtn";
-
+import Signup from "./Signup";
+import RTE from "./RTE";
+import Login from "./Login";
+import Post from "./Post";
 export  {
     Header,
     Footer,
     Container,
     Logo,
     LogOutBtn,
+    RTE,
+    Signup,
+    Login,
+    Post,
 }
